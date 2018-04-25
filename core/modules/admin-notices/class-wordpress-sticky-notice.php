e9e456692abde6fd80541f0d4cf946ffe4cfcae9
Varun Sridharan <varunsridharan23@gmail.com>
2018-04-25 14:21:12 +0530

    * Updated HTML * Updated Script

--- core/modules/admin-notices/class-wordpress-sticky-notice.php
+++ core/modules/admin-notices/class-wordpress-sticky-notice.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class WordPressSticky
  *
@@ -8,6 +9,7 @@
  * @license   GPLV3 Or Greater
  */
 class VSP_WordPress_Sticky_Notice implements VSP_WP_Admin_Notice_Interface {
+
 	/**
 	 * @param VSP_WP_Notice $notice
 	 *
@@ -16,50 +18,48 @@ class VSP_WordPress_Sticky_Notice implements VSP_WP_Admin_Notice_Interface {
 	 * @since  TODO ${VERSION}
 	 */
 	public function formatOutput( VSP_WP_Notice $notice ) {
-		$unqId = uniqid( preg_replace( '/[^a-z0-9A-Z]/', '', $notice->getId() ) );
-		$out   = "
-		<div style=\"position: relative;\" class=\"{$notice->getType()}\">
-			<h4 style=\"margin-top: 4px; margin-bottom: 0;\">{$notice->getTitle()}</h4>
-			<p>
-				{$notice->getContent()}
-				<a id=\"{$unqId}\" href=\"#\" style=\"font-size: 150%; position: absolute; right: 5px; top: -5px; text-decoration: none;\">×</a>
-			</p>
-		</div>
-		";
+		$ui_id = uniqid( preg_replace( '/[^a-z0-9A-Z]/', '', $notice->getId() ) );
+		$out   = '<div style="position: relative;" class="' . $notice->getType() . '">';
 
-		$out .= $this->dismissibleScript( $notice, $unqId );
-
+		$out .= '<h4 style="margin-top: 4px; margin-bottom: 0;">' . $notice->getTitle() . '</h4>';
+		$out .= '<p>';
+		$out .= $notice->getContent();
+		$out .= '<a id="' . $ui_id . '" href="' . $this->get_url( $notice ) . '" style="font-size: 150%; position: absolute; right: 5px; top: -5px; text-decoration: none;">×</a>';
+		$out .= '</p>';
+		$out .= '</div>';
+		$out .= $this->dismissibleScript( $notice, $ui_id );
 		return $out;
+	}
+
+	protected function get_url( VSP_WP_Notice $notice ) {
+		return vsp_ajax_url( array(
+			'action' => VSP_WP_Admin_Notices::KILL_STICKY_NTC_AJAX_ACTION,
+			
+			VSP_WP_Admin_Notices::KILL_STICKY_NTC_AJAX_NTC_ID_VAR => $notice->getId(),
+			VSP_WP_Admin_Notices::KILL_STICKY_NTC_AJAX_NONCE_VAR  => wp_create_nonce( VSP_WP_Admin_Notices::KILL_STICKY_NTC_AJAX_ACTION ),
+		) );
 	}
 
 	/**
 	 * @param VSP_WP_Notice $notice
-	 * @param string        $unqId
+	 * @param string        $ui_id
 	 *
 	 * @return string
 	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
 	 * @since  TODO ${VERSION}
 	 */
-	protected function dismissibleScript( VSP_WP_Notice $notice, $unqId ) {
+	protected function dismissibleScript( VSP_WP_Notice $notice, $ui_id ) {
 		return '
 		<script type="text/javascript">
-			jQuery(document).ready(function ($) {
-				var data = {
-					"action": "' . VSP_WP_Admin_Notices::KILL_STICKY_NTC_AJAX_ACTION . '",
-					"' . VSP_WP_Admin_Notices::KILL_STICKY_NTC_AJAX_NTC_ID_VAR . '": "' . $notice->getId() . '",
-					"' . VSP_WP_Admin_Notices::KILL_STICKY_NTC_AJAX_NONCE_VAR . '": "' . wp_create_nonce( VSP_WP_Admin_Notices::KILL_STICKY_NTC_AJAX_ACTION ) . '"
-				};
-
-				var $notice = $("#' . $unqId . '").parent().parent();
-				$("#' . $unqId . '").click(function(){
-					jQuery.post(ajaxurl, data,
-						function(){
-							$notice.slideUp();
-						}
-					);
-				});
-			});
-		</script>
+		jQuery(document).on("ready",function(){
+			jQuery("#' . $ui_id . '").click(function(e){
+				e.preventDefault();				
+				var $notice = jQuery("#' . $ui_id . '").parent().parent();
+				jQuery.post(jQuery(this).attr("href"),"",function(){$notice.slideUp();});
+			})
+		})
+		</script> 
 		';
+
 	}
 }
