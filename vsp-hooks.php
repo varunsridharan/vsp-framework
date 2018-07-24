edd30f87d6f0b4655c5722fb5a07f8c97d9fcf70
Varun Sridharan <varunsridharan23@gmail.com>
2018-07-24 15:30:46 +0530

    removed unwanted js & css

--- vsp-hooks.php
+++ vsp-hooks.php
@@ -36,41 +36,33 @@ if ( ! function_exists( 'vsp_register_assets' ) ) {
 	function vsp_register_assets() {
 
 		$js = [
-			'blockui'     => vsp_debug_file( 'vendors/blockui/jquery.blockui.js', 'assets' ),
-			'simscroll'   => vsp_debug_file( 'vendors/simscroll/simscroll.js', 'assets' ),
-			'vspajax'     => vsp_debug_file( 'vendors/vspajax/jquery.vsp-ajax.js', 'assets' ),
-			'fancybox'    => vsp_debug_file( 'vendors/fancybox/jquery.fancybox.js', 'assets' ),
-			'addons'      => vsp_debug_file( 'vsp-addons.js', 'js' ),
-			'plugins'     => vsp_debug_file( 'vsp-plugins.js', 'js' ),
-			'framework'   => vsp_debug_file( 'vsp-framework.js', 'js' ),
-			'vuejs'       => vsp_js( 'vue.min.js' ),
-			'lodash'      => vsp_js( 'lodash.min.js' ),
-			'wp-js-hooks' => vsp_debug_file( 'assets/vendors/wp-js-hooks/wp-js-hooks.min.js', 'url' ),
-			'utils'       => vsp_debug_file( 'assets/js/vsp-utils.js', 'url' ),
+			'vspajax'   => vsp_debug_file( 'vendors/vspajax/jquery.vsp-ajax.js', 'assets' ),
+			'fancybox'  => vsp_url( 'assets/vendors/fancybox/jquery.fancybox.min.js', true ),
+			'addons'    => vsp_js( 'vsp-addons.js', true ),
+			'plugins'   => vsp_js( 'vsp-plugins.js', true ),
+			'framework' => vsp_js( 'vsp-framework.js', true ),
+			'vuejs'     => vsp_js( 'vue.min.js' ),
+			'lodash'    => vsp_js( 'lodash.min.js' ),
 		];
 
 		$css = [
-			'framework' => vsp_debug_file( 'vsp-framework.css', 'css' ),
-			'plugins'   => vsp_debug_file( 'vsp-plugins.css', 'css' ),
-			'fancybox'  => vsp_debug_file( 'vendors/fancybox/jquery.fancybox.css', 'assets' ),
-			'addons'    => vsp_debug_file( 'vsp-addons.css', 'css' ),
+			'framework' => vsp_css( 'vsp-framework.css', true ),
+			#'plugins'   => vsp_css( 'vsp-plugins.css', true ),
+			'fancybox'  => vsp_url( 'assets/vendors/fancybox/jquery.fancybox.min.css', true ),
+			'addons'    => vsp_css( 'vsp-addons.css', true ),
 		];
 
-		vsp_register_script( 'wp-js-hooks', $js['wp-js-hooks'], [], '1.0', false );
-		vsp_register_script( 'vsp-simscroll', $js['simscroll'], [ 'jquery' ], '1.3.8', true );
-		vsp_register_script( 'vsp-blockui', $js['blockui'], [ 'jquery' ], '1.0.16', true );
 		vsp_register_script( 'vsp-ajax', $js['vspajax'], [ 'jquery' ], '1.0', true );
 		vsp_register_script( 'vsp-plugins', $js['plugins'], [ 'jquery' ], '1.0', true );
 		vsp_register_script( 'vuejs', $js['vuejs'], [], '1.3.8', true );
 		vsp_register_script( 'lodash', $js['lodash'], [], '1.3.8', true );
-		vsp_register_script( 'vsp-addons', $js['addons'], [ 'vuejs', 'lodash' ], '1.0', false );
+		vsp_register_script( 'vsp-addons', $js['addons'], [ 'vuejs', 'lodash', 'jquery' ], '1.0', false );
 		vsp_register_script( 'vsp-framework', $js['framework'], [ 'jquery' ], '1.0', true );
-		vsp_register_style( 'vsp-plugins', $css['plugins'] );
+		#vsp_register_style( 'vsp-plugins', $css['plugins'] );
 		vsp_register_style( 'vsp-framework', $css['framework'], [], '1.0' );
 		vsp_register_style( 'vsp-addons', $css['addons'], [], '1.0' );
 		vsp_register_style( 'vsp-fancybox', $css['fancybox'], [], '1.0' );
 		vsp_register_script( 'vsp-fancybox', $js['fancybox'], [ 'jquery' ], '1.0.16', true );
-		vsp_register_script( 'vsp-utils', $js['utils'], [ 'jquery', 'wp-js-hooks', 'vsp-blockui' ], '1.0', true );
 	}
 }
 
