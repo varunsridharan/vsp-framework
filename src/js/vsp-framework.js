2d345d22b8798fc9d7c82dc4f5315d06ab020d2c
Varun Sridharan <varunsridharan23@gmail.com>
2018-10-05 11:19:32 +0530

    added js function handler.

--- src/js/vsp-framework.js
+++ src/js/vsp-framework.js
@@ -1,5 +1,21 @@
+/**
+ * Validates And Handles JS Functions.
+ * @param $data
+ * @returns {*}
+ */
+function vsp_js_function ( $data ) {
+	var $e = jQuery.VSPFRAMEWORK.validate_js_function( $data );
+	return $e;
+}
+
+
 ( function ( $, window, document ) {
-	$.VSPFRAMEWORK                  = $.VSPFRAMEWORK || {};
+	$.VSPFRAMEWORK = $.VSPFRAMEWORK || {};
+
+	/**
+	 * Handles Inline Ajax.
+	 * @param e
+	 */
 	$.VSPFRAMEWORK.init_inline_ajax = function ( e ) {
 		e.preventDefault();
 
@@ -19,6 +35,42 @@
 		new VSPAjax( { url: url, method: method }, { trigger_code: trigger_code, element: $this, element_lock: true } );
 	};
 
+
+	/**
+	 * Converts Simple function string into JS functions.
+	 * @param $string
+	 * @returns {*}
+	 */
+	$.VSPFRAMEWORK.validate_single_function = function ( $string ) {
+		if ( typeof $string === 'object' && $string[ 'js_args' ] !== undefined || $string[ 'js_contents' ] !== undefined ) {
+			var $args     = ( $string[ 'js_args' ] === false ) ? false : $string[ 'js_args' ];
+			var $contents = ( $string[ 'js_contents' ] === false ) ? false : $string[ 'js_contents' ];
+			if ( $args === false && $contents !== false ) {
+				return new Function( $contents );
+			} else if ( $args !== false && $contents !== false ) {
+				return new Function( $args, $contents );
+			} else {
+				return $string;
+			}
+		}
+		return $string;
+	};
+
+	/**
+	 * Handles a array of data to check if there any function string that needs to be converted.
+	 * @param $data
+	 * @returns {*}
+	 */
+	$.VSPFRAMEWORK.validate_js_function = function ( $data ) {
+		if ( typeof $data === 'object' || $data === 'array' ) {
+			for ( var $_d in $data ) {
+				$data[ $_d ] = $.VSPFRAMEWORK.validate_single_function( $data[ $_d ] );
+			}
+		}
+		return $data;
+
+	};
+
 	$( document ).ready( function () {
 		if ( $( "#vsp-sys-status-report-text-btn" ).length > 0 ) {
 			$( "#vsp-sys-status-report-text-btn" ).click( function ( e ) {
@@ -32,3 +84,4 @@
 		$( "body" ).on( "click", 'a.vsp-inline-ajax, button.vsp-inline-ajax', $.VSPFRAMEWORK.init_inline_ajax );
 	} );
 } )( jQuery, window, document );
+
