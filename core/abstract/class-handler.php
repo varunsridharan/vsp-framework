f7139f9fca32154381bf8e2005b482835cafbfd9
Varun Sridharan <varunsridharan23@gmail.com>
2018-10-05 11:15:02 +0530

    moved instance handler to another php file.

--- core/abstract/class-handler.php
+++ core/abstract/class-handler.php
@@ -23,21 +23,7 @@ if ( ! class_exists( 'VSP_Class_Handler' ) ) {
 	 * @author Varun Sridharan <varunsridharan23@gmail.com>
 	 * @since 1.0
 	 */
-	class VSP_Class_Handler {
-		/**
-		 * Stores all plugins instances
-		 *
-		 * @var array
-		 */
-		private static $_instances = array();
-
-		/**
-		 * Stores plugins class instances
-		 *
-		 * @var array
-		 */
-		protected $instances = array();
-
+	class VSP_Class_Handler extends VSP_Class_Instance_Handler {
 		/**
 		 * Text_domain
 		 *
@@ -123,36 +109,6 @@ if ( ! class_exists( 'VSP_Class_Handler' ) ) {
 		);
 
 		/**
-		 * Clone
-		 */
-		public function __clone() {
-			vsp_doing_it_wrong( __FUNCTION__, __( 'Cloning instances of the class is forbidden.', 'vsp-framework' ), $this->option( 'version' ) );
-		}
-
-		/**
-		 * WakeUp
-		 */
-		public function __wakeup() {
-			vsp_doing_it_wrong( __FUNCTION__, __( 'Unserializing instances of the class is forbidden.', 'vsp-framework' ), $this->option( 'version' ) );
-		}
-
-		/**
-		 * Returns Current Instance / create a new instance
-		 *
-		 * @return mixed
-		 */
-		public static function instance() {
-			if ( ! isset( self::$_instances[ static::class ] ) ) {
-				$args = func_get_args();
-				$arg1 = ( isset( $args[0] ) && ! empty( $args[0] ) ) ? $args[0] : array();
-				$arg2 = ( isset( $args[1] ) && ! empty( $args[1] ) ) ? $args[1] : array();
-
-				self::$_instances[ static::class ] = new static( $arg1, $arg2 );
-			}
-			return self::$_instances[ static::class ];
-		}
-
-		/**
 		 * Sets Core Values like (plugin_slug,db_slug,hook_slug) and more
 		 *
 		 * @param string $key .
@@ -185,58 +141,6 @@ if ( ! class_exists( 'VSP_Class_Handler' ) ) {
 		}
 
 		/**
-		 * Gets Given key's instance
-		 *
-		 * @param string $key .
-		 *
-		 * @return bool|mixed
-		 */
-		protected function get_instance( $key ) {
-			return ( isset( $this->instances[ $key ] ) ) ? $this->instances[ $key ] : false;
-		}
-
-		/**
-		 * Returns all instance
-		 *
-		 * @return array
-		 */
-		protected function get_all_instances() {
-			return $this->instances;
-		}
-
-		/**
-		 * Creats a new instance for a given class
-		 *
-		 * @param string $key
-		 * @param        $instance
-		 */
-		protected function set_instance( $key = '', $instance ) {
-			$this->instances[ $key ] = $instance;
-		}
-
-		/**
-		 * @param string $class .
-		 * @param bool   $force_instance .
-		 * @param bool   $with_args .
-		 * @param array  $extra_option .
-		 *
-		 * @return object|VS_WP_Endpoint
-		 */
-		public function _instance( $class, $force_instance = false, $with_args = true, $extra_option = array() ) {
-			if ( $this->get_instance( $class ) === false ) {
-				$args = ( true === $with_args ) ? $this->get_common_args( $extra_option ) : $extra_option;
-
-				if ( true === $force_instance && method_exists( $class, 'instance' ) ) {
-					$this->set_instance( $class, $class::instance( $args ) );
-				} else {
-					$instances = new $class( $args );
-					$this->set_instance( $class, $instances );
-				}
-			}
-			return $this->get_instance( $class );
-		}
-
-		/**
 		 * VSP_Class_Handler constructor.
 		 *
 		 * @param array $options .
@@ -254,7 +158,7 @@ if ( ! class_exists( 'VSP_Class_Handler' ) ) {
 		 *
 		 * @return array
 		 */
-		protected function parse_args( $new = array(), $defaults ) {
+		protected function parse_args( $new = array(), $defaults = array() ) {
 			if ( ! is_array( $new ) ) {
 				$new = array();
 			}
@@ -264,7 +168,7 @@ if ( ! class_exists( 'VSP_Class_Handler' ) ) {
 		/**
 		 * Returns $this->file
 		 *
-		 * @return null
+		 * @return string
 		 */
 		public function file() {
 			return $this->file;
@@ -285,7 +189,7 @@ if ( ! class_exists( 'VSP_Class_Handler' ) ) {
 		 *
 		 * @param string $type .
 		 *
-		 * @return bool|null
+		 * @return string|bool
 		 */
 		public function slug( $type = 'slug' ) {
 			$return = false;
