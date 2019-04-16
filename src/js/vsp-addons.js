813140d6b1302a11bdec591d6f2ccf3fed583e34
Varun <varunsridharan23@gmail.com>
2019-04-16 17:30:44 +0530

    Updated

--- src/js/vsp-addons.js
+++ src/js/vsp-addons.js
@@ -1,83 +1,79 @@
-( function ( $, window, document ) {
+/* global vsp_addons_settings:true */
+( function( $, window, document ) {
 	'use strict';
 
 	$( 'div.wponion-framework.wponion-framework-bootstrap' ).removeClass( 'wponion-framework-bootstrap' );
 	$( 'div.wponion-form-actions' ).remove();
 
-	$.VSP_ADDONS         = $.VSP_ADDONS || {};
-	var $vsp_addons_list = new Vue( {
-		el: "#vspAddonListing",
+	$.VSP_ADDONS = $.VSP_ADDONS || {};
+	new Vue( {
+		el: '#vspAddonListing',
 		template: '#VSPAddonsListingTemplate',
 		methods: {
-			getPluginStatusLabel: function ( label ) {
-				if ( this.status[ label ] !== undefined ) {
+			getPluginStatusLabel: function( label ) {
+				if( this.status[ label ] !== undefined ) {
 					return this.status[ label ];
 				}
 			},
-			change_category: function ( cat ) {
+			change_category: function( cat ) {
 				this.current_category = cat;
 			},
-			is_show_cateogry: function ( addon ) {
-				if ( this.current_category === 'all' ) {
+			is_show_cateogry: function( addon ) {
+				if( this.current_category === 'all' ) {
 					return true;
-				} else if ( this.current_category === 'inactive' && addon.is_active === false ) {
+				} else if( this.current_category === 'inactive' && addon.is_active === false ) {
 					return true;
-				} else if ( this.current_category === 'active' && addon.is_active === true ) {
+				} else if( this.current_category === 'active' && addon.is_active === true ) {
 					return true;
-				} else if ( addon.category !== undefined ) {
-					if ( addon.category[ this.current_category ] !== undefined ) {
+				} else if( addon.category !== undefined ) {
+					if( addon.category[ this.current_category ] !== undefined ) {
 						return true;
 					}
 				}
 
 				return false;
 			},
-			pluginViewUrl: function ( addon, file ) {
-				var $data = this.text.plugin_view_url,
-					$data = $data.replace( '{{slug}}', file ),
-					$data = $data.replace( '{{addon.addon_path_md5}}', addon.addon_path_md5 );
-				return $data;
+			pluginViewUrl: function( addon, file ) {
+				var $data = this.text.plugin_view_url;
+				$data     = $data.replace( '{{slug}}', file );
+				return $data.replace( '{{addon.addon_path_md5}}', addon.addon_path_md5 );
 			},
-			addonHandleButton: function ( addon, file, $type ) {
+			addonHandleButton: function( addon, file, $type ) {
 				var $this = this;
 				$.VSP_ADDONS.blockUI( addon.addon_path_md5 );
-				var $parentDIV = jQuery( 'div#' + addon.addon_path_md5 );
-				var $type      = $type;
-				new VSPAjax( {
-					method: 'post',
+				new window.wponion.ajaxer( {
+					method: 'POST',
 					data: {
 						hook_slug: vsp_addons_settings.hook_slug,
 						addon_slug: file,
 						addon_action: $type,
 						addon_pathid: addon.addon_path_md5,
-						action: "vsp_addon_action",
+						action: 'vsp_addon_action',
 					},
-					OnAlways: function () {
+					always: function() {
 						$.VSP_ADDONS.unblock( addon.addon_path_md5 );
 					},
-					OnSuccess: function ( response ) {
-						if ( true === response.success ) {
-							if ( $type === 'activate' ) {
-								$this.pdata[ file ].is_active = true;
-							} else {
-								$this.pdata[ file ].is_active = false;
-							}
+					success: function() {
+						if( $type === 'activate' ) {
+							$this.pdata[ file ].is_active = true;
+						} else {
+							$this.pdata[ file ].is_active = false;
 						}
 					}
 				} );
 			}
 		},
 		computed: {
-			cats: function () {
-				if ( this.categoires === undefined ) {
+			cats: function() {
+				if( this.categoires === undefined ) {
 					var $arr  = this.pdata;
 					var $cats = this.default_cats;
 					var $e    = '';
-					for ( $e in $arr ) {
-						if ( typeof $arr[ $e ].category !== undefined ) {
+					for( $e in $arr ) {
+						if( typeof $arr[ $e ].category !== undefined ) {
 							var $c = '';
-							for ( $c in $arr[ $e ].category ) {
-								if ( $cats[ $c ] === undefined ) {
+							for( $c in $arr[ $e ].category ) {
+								if( $cats[ $c ] === undefined ) {
 									$cats[ $c ] = $arr[ $e ].category[ $c ];
 								}
 							}
@@ -101,9 +97,8 @@
 			text: vsp_addons_settings.texts,
 		},
 	} );
-	$.VSP_ADDONS.blockUI = function ( id ) {
-		$( 'div#' + id ).toggleClass( "vsp-requested" );
-		$( 'div#' + id ).block( {
+	$.VSP_ADDONS.blockUI = function( id ) {
+		$( 'div#' + id ).toggleClass( 'vsp-requested' ).block( {
 			message: null,
 			overlayCSS: {
 				background: '#fff',
@@ -111,9 +106,8 @@
 			}
 		} );
 	};
-	$.VSP_ADDONS.unblock = function ( id ) {
-		$( 'div#' + id ).toggleClass( "vsp-requested" );
-		$( 'div#' + id ).unblock();
+	$.VSP_ADDONS.unblock = function( id ) {
+		$( 'div#' + id ).toggleClass( 'vsp-requested' ).unblock();
 	};
 
 } )( jQuery, window, document );
--- src/js/vsp-framework.js
+++ src/js/vsp-framework.js
@@ -4,29 +4,6 @@ module.exports = ( ( $, window, document, wp ) => {
 
 	$.VSP_FRAMEWORK = {};
 
-	$.VSP_FRAMEWORK.inline_ajax = ( e ) => {
-		e.preventDefault();
-		let $elem = $( e.currentTarget );
-
-		if( $elem.hasClass( 'disabled' ) || $elem.hasClass( 'in-process' ) ) {
-			return;
-		}
-
-		let url          = $elem.attr( 'href' ),
-			method       = $elem.attr( 'data-method' ),
-			trigger_code = $elem.attr( 'data-triggername' );
-
-		if( method === undefined ) {
-			method = 'GET';
-		}
-
-		new VSPAjax( {
-			url: url, method: method
-		}, {
-			trigger_code: trigger_code, element: $elem, element_lock: true
-		} );
-	};
-
 	$.VSP_FRAMEWORK.log_view_file_change = () => {
 		$( '#vsp-log-view-wrap a.button' ).attr( 'disabled', 'disabled' );
 		$( '.wponion-form' ).removeAttr( 'action' ).submit();
@@ -35,8 +12,6 @@ module.exports = ( ( $, window, document, wp ) => {
 	window.vsp_js_function = ( $data ) => window.vsp_helper.to_js_func( $data );
 
 	$( () => {
-		$( 'body' ).on( 'click', 'a.vsp-inline-ajax, button.vsp-inline-ajax', $.VSP_FRAMEWORK.inline_ajax );
-
 		if( $( '#vsp-log-view-wrap' ).length > 0 ) {
 			$( '#vsp-log-view-wrap' ).on( 'change', '.log-header select', $.VSP_FRAMEWORK.log_view_file_change );
 		}
