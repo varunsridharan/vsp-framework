daf0c3b671426ea395ef2fe27f68c37f935ad8f5
Varun <varunsridharan23@gmail.com>
2019-10-26 12:42:55 +0530

    Updated VSP Framework static autoloader.

---
+++ vsp-classmaps.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Last Updated: Sat 26-Oct-2019 / 12:42:26:pm
+ * Total Class:  35
+ * Namespace: 
+ */
+
+return array (
+  'VSP\\Ajax' => 'class-ajax.php',
+  'VSP\\Ajaxer' => 'class-ajaxer.php',
+  'VSP\\Base' => 'class-base.php',
+  'VSP\\Error' => 'class-error.php',
+  'VSP\\Framework_Admin' => 'class-framework-admin.php',
+  'VSP\\Framework_Base' => 'class-framework-base.php',
+  'VSP\\Framework_Modules' => 'class-framework-modules.php',
+  'VSP\\Framework' => 'class-framework.php',
+  'VSP\\Helper' => 'class-helper.php',
+  'VSP\\Setup' => 'class-setup.php',
+  'VSP\\WC_Compatibility' => 'class-wc-compatibility.php',
+  'VSP\\Core\\Abstracts\\Log_Handler' => 'core/abstracts/class-log-handler.php',
+  'VSP\\Core\\Abstracts\\Plugin_Settings' => 'core/abstracts/class-plugin-settings.php',
+  'VSP\\Core\\Instance_Handler' => 'core/class-instance-handler.php',
+  'VSP\\Core\\Interfaces\\Framework_Interface' => 'core/interfaces/framework-interface.php',
+  'VSP\\Core\\Interfaces\\Log_Handler' => 'core/interfaces/log-handler.php',
+  'VSP\\Core\\Interfaces\\Logger' => 'core/interfaces/logger.php',
+  'VSP\\Core\\Traits\\Array_Helper' => 'core/traits/class-array-helper.php',
+  'VSP\\Core\\Traits\\Framework' => 'core/traits/class-framework.php',
+  'VSP\\Core\\Traits\\String_Helper' => 'core/traits/class-string-helper.php',
+  'VSP\\Core\\Traits\\URL' => 'core/traits/class-url.php',
+  'VSP\\Core\\Traits\\WC_Helper' => 'core/traits/class-wc-helper.php',
+  'VSP\\Core\\Traits\\WP' => 'core/traits/class-wp.php',
+  'VSP\\Core\\Traits\\WC_Compatibility\\Product' => 'core/traits/wc-compatibility/trait-product.php',
+  'VSP\\Core\\Traits\\WC_Compatibility\\Version' => 'core/traits/wc-compatibility/trait-version.php',
+  'VSP\\Modules\\Addons\\Admin' => 'modules/addons/class-admin.php',
+  'VSP\\Modules\\Addons\\Core' => 'modules/addons/class-core.php',
+  'VSP\\Modules\\Addons' => 'modules/class-addons.php',
+  'VSP\\Modules\\Logger' => 'modules/class-logger.php',
+  'VSP\\Modules\\Shortcode' => 'modules/class-shortcode.php',
+  'VSP\\Modules\\System_Logs' => 'modules/class-system-logs.php',
+  'VSP\\Modules\\System_Tools' => 'modules/class-system-tools.php',
+  'VSP\\Modules\\WPOnion' => 'modules/class-wponion.php',
+  'VSP\\Modules\\Logger\\File_Handler' => 'modules/logger/class-file-handler.php',
+  'VSP\\Modules\\Logger\\Levels' => 'modules/logger/class-levels.php',
+);
