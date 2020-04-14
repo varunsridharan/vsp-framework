1e7840b0f24b369b7643c72fab018985a56b5c79
Varun <varunsridharan23@gmail.com>
2020-04-14 12:31:58 +0530

    Added VSP_VERSION To Asset Loading.

--- vsp-hooks.php
+++ vsp-hooks.php
@@ -11,8 +11,8 @@ if ( ! function_exists( 'vsp_register_assets' ) ) {
 	 * Registers Basic Framework Styles / Scripts to WP
 	 */
 	function vsp_register_assets() {
-		wp_register_script( 'vsp-framework', vsp_js( 'vsp-framework.js', true ), [ 'wponion-core' ], '1.0', true );
-		wp_register_style( 'vsp-framework', vsp_css( 'vsp-framework.css', true ), [], '1.0' );
+		wp_register_script( 'vsp-framework', vsp_js( 'vsp-framework.js', true ), [ 'wponion-core' ], VSP_VERSION, true );
+		wp_register_style( 'vsp-framework', vsp_css( 'vsp-framework.css', true ), [], VSP_VERSION );
 	}
 }
 
