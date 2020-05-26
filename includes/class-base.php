38ae3733a58dfc414b90c75acc75c77a316d72ba
Varun <varunsridharan23@gmail.com>
2020-05-26 09:16:00 +0530

    Used Class_Options from WPonion

--- includes/class-base.php
+++ includes/class-base.php
@@ -2,31 +2,18 @@
 
 namespace VSP;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	die;
-}
+use WPOnion\Traits\Class_Options;
+
+defined( 'ABSPATH' ) || exit;
 
 if ( ! class_exists( 'Base' ) ) {
 	/**
 	 * Class VSP_Class_Handler
 	 *
 	 * @author Varun Sridharan <varunsridharan23@gmail.com>
-	 * @since 1.0
 	 */
 	class Base extends Core\Instance_Handler {
-		/**
-		 * Options
-		 *
-		 * @var array
-		 */
-		protected $options = array();
-
-		/**
-		 * Default_options
-		 *
-		 * @var array
-		 */
-		protected $default_options = array();
+		use Class_Options;
 
 		/**
 		 * Class Clone.
@@ -42,53 +29,6 @@ if ( ! class_exists( 'Base' ) ) {
 		public function __wakeup() {
 			vsp_doing_it_wrong( __FUNCTION__, __( 'Unserializing instances of the class is forbidden.', 'vsp-framework' ), $this->plugin()
 				->version() );
-		}
-
-		/**
-		 * Merges And sets the given args
-		 *
-		 * @param array $options
-		 * @param array $defaults
-		 */
-		protected function set_args( $options = array(), $defaults = array() ) {
-			$defaults      = empty( $defaults ) ? $this->default_options : $defaults;
-			$this->options = empty( $options ) ? array() : $options;
-			$this->options = $this->parse_args( $this->options, $defaults );
-		}
-
-		/**
-		 * Merges given array
-		 *
-		 * @param array $new
-		 * @param array $defaults
-		 *
-		 * @return array
-		 */
-		protected function parse_args( $new = array(), $defaults = array() ) {
-			$new = ( ! is_array( $new ) ) ? array() : $new;
-			return wp_parse_args( $new, $defaults );
-		}
-
-		/**
-		 * Returns value from options array
-		 *
-		 * @param string $key
-		 * @param bool   $default
-		 *
-		 * @return bool|mixed
-		 */
-		protected function option( $key = '', $default = false ) {
-			return ( isset( $this->options[ $key ] ) ) ? $this->options[ $key ] : $default;
-		}
-
-		/**
-		 * Sets given value for the option
-		 *
-		 * @param string $key
-		 * @param mixed  $value
-		 */
-		protected function set_option( $key, $value ) {
-			$this->options[ $key ] = $value;
 		}
 
 		/**
@@ -108,9 +48,7 @@ if ( ! class_exists( 'Base' ) ) {
 		 * @return string
 		 */
 		public function plugin_url( $ex_path = '/' ) {
-			$file = $this->plugin()
-				->file();
-			return untrailingslashit( plugins_url( $ex_path, $file ) );
+			return untrailingslashit( plugins_url( $ex_path, $this->plugin()->file() ) );
 		}
 
 		/**
@@ -121,9 +59,7 @@ if ( ! class_exists( 'Base' ) ) {
 		 * @return string
 		 */
 		public function plugin_path( $ex_path = '' ) {
-			$file = $this->plugin()
-				->file();
-			$path = untrailingslashit( plugin_dir_path( $file ) );
+			$path = untrailingslashit( plugin_dir_path( $this->plugin()->file() ) );
 			return ( empty( $ex_path ) ) ? $path : $path . '/' . $ex_path;
 		}
 
