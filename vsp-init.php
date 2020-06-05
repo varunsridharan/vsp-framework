13cf034ab9e8f4a8cb0e4590e25165d11cae2f19
Varun <varunsridharan23@gmail.com>
2020-06-05 10:50:48 +0530

    Removed Old WPOnion Hook.

--- vsp-init.php
+++ vsp-init.php
@@ -41,7 +41,6 @@ if ( ! class_exists( 'VSP_Framework_Loader' ) ) {
 		 * VSP_Framework_Loader constructor.
 		 */
 		public function __construct() {
-			add_action( 'wponion_loaded', [ &$this, 'load_framework' ], 0 );
 			add_action( 'wponion/loaded', [ &$this, 'load_framework' ], 0 );
 			add_action( 'vsp_framework_loaded', [ &$this, 'load_plugins' ], -1 );
 			add_action( 'wponion_sysinfo_final', [ &$this, 'add_extra_info' ] );
