aa85d9e2fef64fbfc22e268963a3725fab88a523
Varun <varunsridharan23@gmail.com>
2020-07-10 08:59:36 +0530

    Added Addon Abstract Class.

---
+++ includes/core/abstracts/clss-addon.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace VSP\Core\Abstracts;
+
+use ReflectionClass;
+use VSP\Core\Instance_Handler;
+use WPOnion\Traits\Class_Options;
+use WPOnion\Traits\Hooks;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class VSP_Log_Handler
+ *
+ * @author Varun Sridharan <varunsridharan23@gmail.com>
+ */
+abstract class Addon extends Instance_Handler {
+	use Class_Options;
+	use Hooks;
+
+	/**
+	 * Stors Addon File Location String.
+	 *
+	 * @var string
+	 */
+	private $addon_file = '';
+
+	/**
+	 * Finds And Returns Valid Addon Path File.
+	 *
+	 * @return false|string
+	 */
+	protected function addon_file() {
+		if ( empty( $this->addon_file ) ) {
+			$reflector        = new ReflectionClass( static::class );
+			$this->addon_file = $reflector->getFileName();
+		}
+		return $this->addon_file;
+	}
+
+	/**
+	 * Get the plugin url.
+	 *
+	 * @param string      $ex_path
+	 * @param bool|string $addon_file
+	 *
+	 * @return string
+	 */
+	public function addon_url( $ex_path = '/', $addon_file = false ) {
+		$file = ( false !== $addon_file ) ? $addon_file : $this->addon_file();
+		return untrailingslashit( plugins_url( $ex_path, $file ) );
+	}
+
+	/**
+	 * Get the plugin path.
+	 *
+	 * @param string      $ex_path
+	 * @param bool|string $addon_file
+	 *
+	 * @return string
+	 */
+	public function addon_path( $ex_path = '', $addon_file = false ) {
+		$file = ( false !== $addon_file ) ? $addon_file : $this->addon_file();
+		$path = untrailingslashit( plugin_dir_path( $file ) );
+		return ( empty( $ex_path ) ) ? $path : $path . '/' . $ex_path;
+	}
+
+}
